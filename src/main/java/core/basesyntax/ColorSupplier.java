e2719c63d96a2fcde15b75e04f9e9dbc3b0732ce
Daryna Mitina <mitinadari@gmail.com>
2026-05-09 23:37:01 +0300

    Add class Ball with constructor and toString method. Add class Lottery with the getRandomBall method.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
-public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+import java.util.Random;
+
+public class ColorSupplier extends Lottery {
+    public Colors getRandomColor() {
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index];
     }
 }
