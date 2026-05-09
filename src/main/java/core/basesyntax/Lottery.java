5cb22af611dfad14ba96172a954b125f7ceaae41
Daryna Mitina <mitinadari@gmail.com>
2026-05-10 00:39:42 +0300

    Refactor Lottery class

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int maxNumber = 101;
+    private final ColorSupplier supplier = new ColorSupplier();
+    private final Random random = new Random();
+    private static final int MAX_Ball_NUMBER = 100;
 
     public Ball getRandomBall() {
-        ColorSupplier supplier = new ColorSupplier();
-        Colors color = supplier.getRandomColor();
-        int number = new Random().nextInt(maxNumber);
-        Ball ball = new Ball(color.name(), number);
-        return ball;
+        return new Ball(supplier.getRandomColor().name(), random.nextInt(MAX_Ball_NUMBER + 1));
     }
 }
